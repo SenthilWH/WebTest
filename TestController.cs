c5492f2fed76060a5d47771675f8e0bfb1433a42
SenthilWH <110588291+SenthilWH@users.noreply.github.com>
2022-08-10 11:38:37 +0530

    Add files via upload

---
+++ TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WTGHTest.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            //10082022 senthil test
+            return View();
+        }
+    }
+}
